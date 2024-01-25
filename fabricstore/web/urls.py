e59730b1bd9a194f6211ef886de93cb40ba2d787
mehran latifi <mehran.latifi8383@gmail.com>
2024-01-25 16:47:49 +0330

    Added logout functionality using Django's built-in auth views

--- fabricstore/web/urls.py
+++ fabricstore/web/urls.py
@@ -1,6 +1,7 @@
 # urls.py
 from django.urls import path
 from .views import index, search, user_login, password_login, user_register, user_profile, admin_settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', index, name='index'),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('login/password/<str:phone>/', password_login, name='password_login'),
     path('register/<str:phone>/', user_register, name='user_register'),
     path("setting/", admin_settings, name="admin_settings"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
