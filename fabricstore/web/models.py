465432fc30653ee57a486019f5b02ca00fc94637
mehran latifi <mehran.latifi8383@gmail.com>
2024-01-26 17:52:09 +0330

    Enhance Fabric model with price, description, image fields

--- fabricstore/web/models.py
+++ fabricstore/web/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class Fabric(models.Model):
     name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='fabrics/', blank=True, null=True)
 
     def __str__(self):
         return self.name
